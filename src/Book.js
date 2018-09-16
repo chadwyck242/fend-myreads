415ecac5ab200815518a180c75ef36ae16e0e139
chadwyck242 <chadwyck.codeart@gmail.com>
2018-09-16 15:40:25 -0600

    Start buidling component structure

---
+++ src/Book.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Book(props) {
+
+}
+
+export default Book
---
+++ src/BookMain.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+class BookMain extends Component {
+
+}
+
+export default BookMain
